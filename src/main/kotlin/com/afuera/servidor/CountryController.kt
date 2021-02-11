1e4114f6224514bdd1aec0268dab414745a340bc
RodrigoSantosV <ro.santosvi@gmail.com>
2021-02-11 18:03:13 +0100

    joiji

--- src/main/kotlin/com/afuera/servidor/CountryController.kt
+++ src/main/kotlin/com/afuera/servidor/CountryController.kt
@@ -11,26 +11,12 @@ class CountryController(private val countriesRepository: CountriesRepository) {
         return countriesRepository.findAll()
     }
 
+    @GetMapping("/country/{id}")
+    fun getCountry(@PathVariable id : Long) : Country {
+        return countriesRepository.findById(id).get()
+    }
     @GetMapping("/countCountries")
     fun getCount() : Long {
         return countriesRepository.count()
     }
-
-    @PostMapping("/country")
-    fun insertCountry(@RequestBody student : Country){
-        countriesRepository.save(student)
-    }
-
-    @GetMapping("/country/{id}")
-    fun getCountry(@PathVariable id : Long) : Country {
-        Cargar.logger.info("Se ha recibido un un $id")
-        return countriesRepository.findById(id).get()
-    }
-
-    @DeleteMapping("/country/{id}")
-    fun deleteCountry(@PathVariable id : Long){
-        countriesRepository.deleteById(id)
-    }
-
-
 }
