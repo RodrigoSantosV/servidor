bbcb19f15ee35afdd6cb811728f2adf850786f44
RodrigoSantosV <ro.santosvi@gmail.com>
2021-02-20 22:08:02 +0100

    encriptado

--- src/main/kotlin/com/afuera/servidor/CountryController.kt
+++ src/main/kotlin/com/afuera/servidor/CountryController.kt
@@ -1,6 +1,39 @@
 package com.afuera.servidor
 
 import org.springframework.web.bind.annotation.*
+import java.security.MessageDigest
+import java.util.*
+import javax.crypto.BadPaddingException
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
+
+private fun cifrar(textoEnString : String, llaveEnString : String) : String {
+    println("Voy a cifrar $textoEnString")
+    val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+    cipher.init(Cipher.ENCRYPT_MODE, getKey(llaveEnString))
+    val textCifrado = Base64.getEncoder().encodeToString(cipher.doFinal(textoEnString.toByteArray(Charsets.UTF_8)))
+    println("He obtenido $textCifrado")
+    return textCifrado
+}
+
+@Throws(BadPaddingException::class)
+private fun descifrar(textoCifrrado : String, llaveEnString : String) : String {
+    println("Voy a descifrar $textoCifrrado")
+    val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+    cipher.init(Cipher.DECRYPT_MODE, getKey(llaveEnString));
+    val textDescifrado = String(cipher.doFinal(Base64.getDecoder().decode(textoCifrrado)))
+    println("He obtenido $textDescifrado")
+    return textDescifrado
+}
+
+
+private fun getKey(llaveEnString : String): SecretKeySpec {
+    var llaveUtf8 = llaveEnString.toByteArray(Charsets.UTF_8)
+    val sha = MessageDigest.getInstance("SHA-1")
+    llaveUtf8 = sha.digest(llaveUtf8)
+    llaveUtf8 = llaveUtf8.copyOf(16)
+    return SecretKeySpec(llaveUtf8, "AES")
+}
 
 
 @RestController
@@ -13,10 +46,15 @@ class CountryController(private val countriesRepository: CountriesRepository) {
 
     @GetMapping("/country/{id}")
     fun getCountry(@PathVariable id : Long) : Country {
-        return countriesRepository.findById(id).get()
+        val encriptador = id.toString()
+        val llave = "elpepe"
+        val descifrador = descifrar(cifrar(encriptador,llave),llave)
+
+        return countriesRepository.findById(descifrador.toLong()).get()
     }
     @GetMapping("/countCountries")
     fun getCount() : Long {
         return countriesRepository.count()
     }
+
 }
--- src/main/kotlin/com/afuera/servidor/ServidorApplication.kt
+++ src/main/kotlin/com/afuera/servidor/ServidorApplication.kt
@@ -2,10 +2,16 @@ package com.afuera.servidor
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import java.security.MessageDigest
+import java.util.*
+import javax.crypto.BadPaddingException
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
 
 @SpringBootApplication
 class ServidorApplication
 
 fun main(args: Array<String>) {
 	runApplication<ServidorApplication>(*args)
+
 }
